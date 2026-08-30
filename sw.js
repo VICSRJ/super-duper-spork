b2d01e6a04c9dee4426482ad02b78f710f04c1ab
vics <ruzickajakub95@gmail.com>
2026-08-30 09:46:45 +0200

    feat: add Botanic WMS service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='botanic-wms-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return res}))));
